9529869ec9db87fd5058c47cd2d653145b4fb1d4
Adam <kapelner@gmail.com>
2012-05-15 17:14:21 -0400

    learnopedia as a new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learnopedia::Application.config.secret_token = 'd68cda4d759d46489311bd20f0cba55ad4ad79c2fc3d60f86affef1e7a6d7174ea36a494be2dbdfbee05f10aca394b9f6665db765f16a613e255fb7e412f5e3f'
